e3c0ec1e9f60df12cee575ffd0127aa463399fae
Albert Medina <albertmecu@gmail.com>
2025-10-15 15:38:47 +0200

    feat: added Student class

---
+++ src/main/java/level3/Student.java
@@ -0,0 +1,49 @@
+package level3;
+
+public class Student {
+
+    private String name;
+    private int age;
+    private String course;
+    private double grade;
+
+    public Student(String name, int age, String course, double grade) {
+
+        if (name == null || name.trim().isEmpty() || name.matches(".*\\d.*")) {
+            throw new IllegalArgumentException("Invalid name.");
+        }
+
+        if (age < 0) {
+            throw new IllegalArgumentException("Invalid age.");
+        }
+
+        if (course == null || course.trim().isEmpty()) {
+            throw new IllegalArgumentException("Invalid course.");
+        }
+
+        if (grade < 0 || grade > 10) {
+            throw new IllegalArgumentException("Invalid grade.");
+        }
+
+        this.name = name;
+        this.age = age;
+        this.course = course;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+}
